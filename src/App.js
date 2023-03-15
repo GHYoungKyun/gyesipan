334260380a25d140bd494f3f572b1402e7251b42
GHYoungKyun <dudrbsapdlf@naver.com>
2023-03-16 02:22:04 +0900

    page movement(fail)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import List from './list';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          "Hello world!"
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">LIST</Link>
+                </li>
+            </ul>
+            <hr/>
+            <Route path="/" exact={true} component={List}/>
+        </div>
+    )
 }
 
 export default App;
